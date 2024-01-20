5dc4a1894a32fcd4eb25676fba798158d433bfeb
vivianhan404 <viv.han.12@gmail.com>
2024-01-19 22:03:18 -0500

    create DocText and allow text input

--- client/src/components/pages/Document.js
+++ client/src/components/pages/Document.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DocText from "./DocText";
 
 /**
  * @typedef PageObject
@@ -17,7 +18,7 @@ const Document = (props) => {
     return (
         <div>
             <div>{props.data.prompt}</div>
-            <div>{props.data.content}</div>
+            <DocText content={props.data.content} />
         </div>
     )
 }
