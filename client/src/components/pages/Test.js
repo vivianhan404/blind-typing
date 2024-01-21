8ea3eca9cd6e095511a143bcebeb16211e8f0bc2
vivianhan404 <viv.han.12@gmail.com>
2024-01-21 12:22:42 -0500

    got good formatting for test

--- client/src/components/pages/Test.js
+++ client/src/components/pages/Test.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./Test.css";
 
 const Test = () => {
-  return <div className="Test-background"></div>;
+  return (
+    <div className="Test-background">
+      <div className="Test-header">header</div>
+      <div className="Test-body">body</div>
+    </div>
+  );
 };
 
 export default Test;
